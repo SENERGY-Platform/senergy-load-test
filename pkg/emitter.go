3e8f4e16e4f992407ac247e515c149ac15ee83aa
Ingo <roessner@infai.org>
2021-10-19 09:00:59 +0200

    emitter only with interval > 0

--- pkg/emitter.go
+++ pkg/emitter.go
@@ -12,25 +12,27 @@ type Message struct {
 }
 
 func Emitter(ctx context.Context, out chan<- Message, info map[string]string, interval time.Duration, message func() string) {
-	go func() {
-		//wait for random time between now and interval to offset emitter
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		if interval <= 1<<31-1 {
-			time.Sleep(time.Duration(r.Int31n(int32(interval))))
-		} else {
-			time.Sleep(time.Duration(r.Int63n(int64(interval))))
-		}
-		emit(out, info, message)
-		t := time.NewTicker(interval)
-		for {
-			select {
-			case <-ctx.Done():
-				return
-			case <-t.C:
-				emit(out, info, message)
+	if interval > 0 {
+		go func() {
+			//wait for random time between now and interval to offset emitter
+			r := rand.New(rand.NewSource(time.Now().UnixNano()))
+			if interval <= 1<<31-1 {
+				time.Sleep(time.Duration(r.Int31n(int32(interval))))
+			} else {
+				time.Sleep(time.Duration(r.Int63n(int64(interval))))
 			}
-		}
-	}()
+			emit(out, info, message)
+			t := time.NewTicker(interval)
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				case <-t.C:
+					emit(out, info, message)
+				}
+			}
+		}()
+	}
 }
 
 func emit(out chan<- Message, info map[string]string, message func() string) {
